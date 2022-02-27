c82f2966f4eb55861c182ae97e54ae3a1beb7c12
kiwi0202 <2641841G@student.gla.ac.uk>
2022-02-27 12:55:31 +0000

    update5populate

---
+++ tango_with_django_project/tango_with_django_project/populate_rango.py
@@ -0,0 +1,14 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+
+
+# For an explanation of what is going on here, please refer to the TwD book.
+
+def populate():
+    python_pages = [
+        {'title': 'Official Python Tutorial',
+         'url':'http://docs.python.org/3/tutorial/'},
+        {'title':'How to Think like a Computer Scientist',
+         'url':'http://www.greenteapress.com/thinkpython/'},
+        {'title':'Learn Python in 10 Minutes',
+         'url':'http://www.korokithakis.net/tutorials/python/'} ]
